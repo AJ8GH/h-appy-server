1aa32d2d6a1abfd7d8e2cd9d773b24cdc73e4c5d
Nicolas Bermejo <nicbermejo@gmail.com>
2021-04-13 17:13:21 +0100

    added an activity list to collapsible

--- h-appy/App.js
+++ h-appy/App.js
@@ -2,10 +2,13 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import CollapsibleView from "@eliav2/react-native-collapsible-view";
-import { StyleSheet, Text, View, Button, Alert } from "react-native";
+import { StyleSheet, Text, View, Button, Alert, FlatList} from "react-native";
 
 
 export default function App() {
+  const renderItem = ({ item }) => (
+    <Item title={item.title} />
+  );
   return (
     <View style={styles.container}>
       <Text>Hello Haddocks!</Text>
@@ -15,7 +18,12 @@ export default function App() {
       />
 
       <CollapsibleView title="Nibbles" style={styles.menuCollapsible} noArrow={true}>
-        <Text>little bits</Text>
+        <Text>Nibblelist(Nibbles)</Text>
+        <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
 
     </CollapsibleView>
       <CollapsibleView title="Appetisers" style={styles.menuCollapsible} noArrow={true}>
@@ -34,8 +42,30 @@ export default function App() {
     <StatusBar />
     </View>
   );
+
 }
 
+
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'Go to the Cinema',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Do a Puzzle',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Take a long awaited break',
+  },
+];
+
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
 const styles = StyleSheet.create({
   container: {
     flex: 1,
