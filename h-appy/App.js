df14055a166a58589b15c53628b6dd1452b01c21
Joey <joey.s.hornsby@gmail.com>
2021-04-13 15:54:19 +0100

    added basic collapsible menu

--- h-appy/App.js
+++ h-appy/App.js
@@ -1,13 +1,37 @@
 /* eslint-disable no-use-before-define */
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import CollapsibleView from "@eliav2/react-native-collapsible-view";
+import { StyleSheet, Text, View, Button, Alert } from "react-native";
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello Haddocks!</Text>
-      <StatusBar />
+      <Button
+        title="Fish"
+        onPress={() => Alert.alert('here is a fish')}
+      />
+      
+      <CollapsibleView title="Nibbles">
+        <Text>little bits</Text>
+      
+    </CollapsibleView>
+      <CollapsibleView title="Appetisers">
+        <Text>very tasty small things</Text>      
+    </CollapsibleView>
+    <CollapsibleView title="Main meals">
+
+        <Text>very tasty medium things</Text>
+
+      
+    </CollapsibleView>
+    <CollapsibleView title="Desserts">
+        <Text>pudding</Text>
+      
+    </CollapsibleView>
+    <StatusBar />
     </View>
   );
 }
