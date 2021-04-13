2704556d3be173c53a29a95bef15af4d1b80fd9b
Jack Gumoes <j.gumoes@gmail.com>
2021-04-13 22:16:59 +0100

    added ingredients to the menu items

--- h-appy/App.js
+++ h-appy/App.js
@@ -55,36 +55,43 @@ const DATA = {
     {
       id: 'bd7dcbea-c1b1-46c2-aed5-3ad53abb28ba',
       title: 'Go to the Cinema',
+      ingredients: []
     },
     {
       id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
       title: 'Do a Puzzle',
+      ingredients: ['a phone or computer or puzzle book']
     },
     {
       id: '58694a0f-3da1-471f-bd96-145571e29d72',
       title: 'Take a long awaited break',
+      ingredients: []
     },
   ],
   "Appetisers": [
     {
       id: 'bd7acbea-c1b1-46c2-aed5-3ad53dbb28ba',
       title: 'Do a codewars kata',
+      ingredients: ["computer"]
     },
     {
       id: 'bd0acjea-c4b1-46c2-red5-3ad53abb28ba',
-      title: 'Play a piece of music'
+      title: 'Play a piece of music',
+      ingredients: ['an instrument', 'somewhere private']
     }
   ],
   "Mains": [
     {
       id: 'ai589cm1-oi5n-alf3-bd96-145571e29d72',
-      title: 'Learn a new song on the guitar'
+      title: 'Learn a new song on the guitar',
+      ingredients: ['a guitar']
     }
   ],
   "Desserts": [
     {
       id: '3ac68afc-dk30-3kf9-a4f8-fbd91aa9d07k',
-      title: 'Browse Reddit for 3 hours'
+      title: 'Browse Reddit for 3 hours',
+      ingredients: ['a phone', 'Ennui']
     }
   ]
 };
