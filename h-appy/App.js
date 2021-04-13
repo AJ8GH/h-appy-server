4086455a296b5efa4c9920937d967f0aebd634c0
Jack Gumoes <j.gumoes@gmail.com>
2021-04-13 14:46:12 +0100

    set up linter

--- h-appy/App.js
+++ h-appy/App.js
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/* eslint-disable no-use-before-define */
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello Haddocks!</Text>
-      <StatusBar style="auto" />
+      <StatusBar />
     </View>
   );
 }
@@ -14,8 +15,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
