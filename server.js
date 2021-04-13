88a05b76cbed540cf2ba065c037c631ab99e0bad
Peter Allen <peter_james_allen@hotmail.com>
2021-04-13 15:32:10 +0100

    created js script to insert data into test db

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-require('dotenv').config()
-const express = require('express');
-const mongoose = require('mongoose');
-const activityRouter = require('./routes/activityRoutes.js');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const activityRouter = require("./routes/activityRoutes.js");
 
 const app = express();
 
 app.use(express.json());
 
 mongoose.connect(
-  `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@happyhaddock.e6r3s.mongodb.net/HappyHaddock?retryWrites=true&w=majority`,
+  `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@happyhaddock.e6r3s.mongodb.net/test?retryWrites=true&w=majority`,
   {
     useNewUrlParser: true,
     useFindAndModify: false,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   }
 );
 
 app.use(activityRouter);
 
 app.listen(3000, () => {
-  console.log('Server is running...');
+  console.log("Server is running...");
 });
