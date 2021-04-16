ca4599c832e7d3c198527c1a5e95551ecac0e2ca
Joey <joey.s.hornsby@gmail.com>
2021-04-16 15:41:30 +0100

    added auth middleware

---
+++ app/middleware/auth.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken')
+const userModel = require('../models/user.js')
+
+const auth = async(request, response, next) => {
+  const token = request.header('Authorization').replace('Bearer ', '')
+  try {
+    const data = jwt.verify(token, process.env.JWT_KEY)
+    const user = await userModel.findOne({_id: data._id, 'tokens.token': token })
+    if(!user) {
+      throw new Error()
+    }
+    request.user = user
+    request.token = token
+    next()
+  } catch (error) {
+    response.status(401).send({ error: 'Not authorized to access this resource'})
+  }
+} 
+
+module.exports = auth
--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const User = require('../models/user');
 const userModel = require('../models/user');
 const userApp = express();
+const auth = require('../middleware/auth');
 
 userApp.post('/user', async (request, response) => {
   try {
@@ -17,7 +17,7 @@ userApp.post('/user', async (request, response) => {
 userApp.post('/user/login', async (request, response) => {
   try {
     const { email, password } = request.body
-    const user = await User.findByCredentials(email, password)
+    const user = await userModel.findByCredentials(email, password)
     if (!user) {
       return response.status(401).send({error: 'Login failed! Check authentication credentials'})
     }
@@ -26,6 +26,10 @@ userApp.post('/user/login', async (request, response) => {
   } catch (error) {
     response.status(400).send(error)
   }
+});
+
+userApp.get('/user/profile', auth, async(request, response) => {
+  response.send(request.user)
 })
 
 module.exports = userApp;
