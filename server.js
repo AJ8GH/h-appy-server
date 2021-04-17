1eddf8dcc5b8b9d700414bff4c9c0ec4bbb8d545
Peter Allen <peter_james_allen@hotmail.com>
2021-04-17 16:39:15 +0100

    added procfile and env.PORT to server

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ mongoose.connect(
 app.use(activityRouter);
 app.use(userRouter);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Server is running...');
 });
 
